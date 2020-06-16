d8fe8c9dbc4dcd32e6865f19934ffcd02fc9875b
Kai Michael Poppe <kmpoppe@users.noreply.github.com>
2020-06-16 07:06:08 +0200

    Create index.php

---
+++ index.php
@@ -0,0 +1,20 @@
+<html>
+<head>
+<style type="text/css">
+   body {
+   font-family:'Arial', sans-serif;
+   font-size:10pt;
+ }
+</style>
+</head>
+<body>
+<h1>Changeset Analyse</h1>
+<form action="changeset-analyzer.php" method="post" onsubmit="subm.disabled = true; return true;">
+   Gib&apos; hier deinen OSM-Benutzernamen ein:
+<input type="text" name="display"/>
+<input type="submit" value="starten" name='subm'/>
+</form>
+<h2>Achtung!</h2>
+   Das Abholen der Daten aus der OSM API dauert eine (ganze) Weile. Bitte ein wenig Geduld!
+</body>
+</html>
